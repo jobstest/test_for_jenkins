3c81b96fe0b0c1b60d136182719deba583b2be1e
Vladimir Parfionov <jobstest18@gmail.com>
2023-03-24 15:27:17 +0400

    Initial commit

---
+++ src/test/java/ExampleTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class ExampleTests {
+
+    @Test
+    void exmple(){
+        open("https://demowebshop.tricentis.com");
+    }
+
+}
